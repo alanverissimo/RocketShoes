98c0be59ccb63e3fdc965908a6ac9cbb2a2f3d9a
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-05 15:55:25 -0300

    rootReducer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global'
 import Header from './components/Header';
+import { Provider } from 'react-redux';
 
 import Routes from './routes';
+
+import store from './store';
 
 
 function App() {
     return (
+        <Provider store ={store}>
         <BrowserRouter>
         <Header />
         <Routes />
         <GlobalStyle />
         </BrowserRouter>
+        </Provider>
     )
        
     
