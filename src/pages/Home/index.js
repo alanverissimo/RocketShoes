ba34d2c0faf8c6a83dbd7fd72aff13f4eaf34288
Alan Verissimo <alan_verissimo@hotmail.com>
2022-06-05 01:45:59 -0300

    Second Commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,81 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Carrinho</h1>
+    return ( <ProductList>
+        <li>
+            <img src="https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(95)/undeaeoo/catalog/nike/nike-air-force-1-07-triple-white-cw2288-111-yythkg.jpg" alt="Tênis"/>
+        
+        <strong>Tênis Confortavel</strong>
+        <span>R$180,00</span>
+
+        <button type="button">
+            <div>
+            <MdAddShoppingCart size={16} color="FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+        </button>
+        </li>
+
+        <li>
+            <img src="https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(95)/undeaeoo/catalog/nike/nike-air-force-1-07-triple-white-cw2288-111-yythkg.jpg" alt="Tênis"/>
+        
+        <strong>Tênis Confortavel</strong>
+        <span>R$180,00</span>
+
+        <button type="button">
+            <div>
+            <MdAddShoppingCart size={16} color="FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+        </button>
+        </li>
+
+        <li>
+            <img src="https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(95)/undeaeoo/catalog/nike/nike-air-force-1-07-triple-white-cw2288-111-yythkg.jpg" alt="Tênis"/>
+        
+        <strong>Tênis Confortavel</strong>
+        <span>R$180,00</span>
+
+        <button type="button">
+            <div>
+            <MdAddShoppingCart size={16} color="FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+        </button>
+        </li>
+
+        <li>
+            <img src="https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(95)/undeaeoo/catalog/nike/nike-air-force-1-07-triple-white-cw2288-111-yythkg.jpg" alt="Tênis"/>
+        
+        <strong>Tênis Confortavel</strong>
+        <span>R$180,00</span>
+
+        <button type="button">
+            <div>
+            <MdAddShoppingCart size={16} color="FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+        </button>
+        </li>
+
+        <li>
+            <img src="https://img.irroba.com.br/fit-in/600x600/filters:format(webp):fill(fff):quality(95)/undeaeoo/catalog/nike/nike-air-force-1-07-triple-white-cw2288-111-yythkg.jpg" alt="Tênis"/>
+        
+        <strong>Tênis Confortavel</strong>
+        <span>R$180,00</span>
+
+        <button type="button">
+            <div>
+            <MdAddShoppingCart size={16} color="FFF" /> 3
+            </div>
+            <span>ADICIONAR AO CARRINHO</span>
+        </button>
+        </li>
+        </ProductList>
+
+    );
     
 }
